4822714d285166a17a150baf095eb87c609afc61
Chirstopher Ottley <dev@referencelogic>
2013-12-07 19:05:10 -0400

    Changed map navigation to use touch controls

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,12 @@ function getLocation() {
     angular.element(document.getElementById("policeStationList")).scope().updateDistances();
     angular.element(document.getElementById("policeStationList")).scope().$apply();
     var closestStation = angular.element(document.getElementById("policeStationList")).scope().closestStationUsingGPS();
+    
+    if ( (closestStationLonCheck != closestStation[0].txtlon) || (closestStationLatCheck != closestStation[0].txtlat ) ) {
+
+    closestStationLonCheck = closestStation[0].txtlon;
+    closestStationLatCheck = closestStation[0].txtlat;
+        
     var closestStationHTML = '<a href="#displayonmap" class="theme theme-none locationafter" id="aflink">' + closestStation[0].station + '<br/>' + closestStation[0].address + '<br/>Approx Distance: ' + closestStation[0].distance + ' KM</a><br/>';
     var closestStationMapHTML = '<u>Closest Police Station:</u> ' + closestStation[0].station + '<br/>' + closestStation[0].address + '<br/>Approx Distance: ' + closestStation[0].distance + ' KM<br/>';
     var closestStationTelephoneHTML = '<a href="tel:' + closestStation[0].telephone + '" class="icon phone" data-transition="none">Call ' +  closestStation[0].telephone + '</a>';
@@ -16,19 +22,19 @@ function getLocation() {
     document.getElementById("closestpolicestationdataformap").innerHTML = closestStationMapHTML;
     
     var stationPosition = new OpenLayers.LonLat(closestStation[0].txtlon,closestStation[0].txtlat).transform('EPSG:4326', 'EPSG:900913');
-      
-    markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
- 
+     
+    markers.clearMarkers();
     markers.addMarker(new OpenLayers.Marker(stationPosition));
  
     map.setCenter (stationPosition, 15);
+    
+    }
       
   };
   var locFail = function() {
 	  document.getElementById("textinputlat").value = 'Unable to get latitude';
 	  document.getElementById("textinputlon").value = 'Unable to get longitude';
-      document.getElementById("closestpolicestationdata").innerHTML = '<a class="theme theme-none" id="aflink" href="#displayonmap">No GPS detected</a>';
+      document.getElementById("closestpolicestationdata").innerHTML = 'No GPS detected';
       document.getElementById("closestpolicestationdataformap").innerHTML = "No station identified";
   }; 
   navigator.geolocation.getCurrentPosition(suc, locFail);
@@ -46,17 +52,16 @@ function init() {
       'OK,Cancel');
     }, true);     
 
-
-  Hammer(document.getElementById('main')).on("swipeleft", function(event) { $.ui.loadContent("#stations"); });
-  Hammer(document.getElementById('main')).on("swiperight", function(event) { $.ui.loadContent("#about",false,true,"slide"); });
-    
-  Hammer(document.getElementById('stations')).on("swipeleft", function(event) { $.ui.loadContent("#about"); });
-  Hammer(document.getElementById('stations')).on("swiperight", function(event) { $.ui.loadContent("#main",false,true,"slide"); });
-
-  Hammer(document.getElementById('about')).on("swipeleft", function(event) { $.ui.loadContent("#main"); });
-  Hammer(document.getElementById('about')).on("swiperight", function(event) { $.ui.loadContent("#stations",false,true,"slide"); });
-    
-  map = new OpenLayers.Map("map");
+  map = new OpenLayers.Map({div: "map",
+    controls: [
+        new OpenLayers.Control.TouchNavigation({
+            dragPanOptions: {
+                interval: 0, // non-zero kills performance on some mobile phones
+                enableKinetic: true
+            }
+        }),
+        new OpenLayers.Control.ZoomPanel()
+    ]});
   var mapnik         = new OpenLayers.Layer.OSM();
   var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
   var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
@@ -65,6 +70,8 @@ function init() {
  
   map.addLayer(mapnik);
   map.setCenter(position, zoom );
+  markers = new OpenLayers.Layer.Markers( "Markers" );
+  map.addLayer(markers);
 
   getLocation();
   setInterval(function() { getLocation(); }, 10000);
